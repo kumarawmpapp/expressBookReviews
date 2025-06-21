82e0b72af70f806ffa107b33b352502703e72144
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-06-22 03:28:42 +0530

    Update auth_users.js
     Task 9 – Delete Book Review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,6 +56,20 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(200).send("Review added/updated");
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    if (books[isbn].reviews && books[isbn].reviews[username]) {
+        delete books[isbn].reviews[username];
+        return res.status(200).send("Review deleted");
+    }
+
+    return res.status(404).send("Review not found");
+});
+
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
