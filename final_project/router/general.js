323d933e6ad02cf8293999f9b476f61e109c3664
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-06-22 03:21:37 +0530

    Update general.js
    Task 5 – Get Book Review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,9 +50,9 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
