0f9a1170aed0287423f6bf3597a35f632e3a678c
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-06-22 03:20:15 +0530

    Update general.js
    Task 3 – Get Book by Author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,9 +22,17 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const results = [];
+
+    for (let key in books) {
+        if (books[key].author === author) {
+            results.push(books[key]);
+        }
+    }
+
+    res.status(200).json(results);
 });
 
 // Get all books based on title
